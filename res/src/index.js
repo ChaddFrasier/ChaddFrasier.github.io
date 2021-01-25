667a158677a34697deab1ead97db89bda317f4c5
ChaddFrasier <chaddfrasier20@gmail.com>
2021-01-24 17:11:55 -0700

    first major update complete!

---
+++ res/src/index.js
@@ -0,0 +1,47 @@
+
+'use strict';
+
+/**
+ * News Line Data:
+ * ---------------
+ *      This JSON object will be updated whenever a new headline is added to the homepage.
+ *      There should not be two of the same dates ever so that is why they are the keys
+ */
+const NewsData = {
+    '10/22/2020': "Contract Extension Awarded for Student Contractor position at the USGS.",
+    '08/17/2019': "Started drafting research methods for Machine Learning Techniques Applied to Lunar Surface Data with Dr. Kestay at the USGS.",
+    '07/03/2019': "First day at the USGS as a Student Software Contractor.",
+    '06/12/2019': "Graduated from Northern Arizona University with a Bachelor's of Science in Computer Science.",
+    '06/09/2018': "Assistant Teacher at Kids Coding Camp for College America."
+};
+
+// Function executes when the page loads fully
+document.addEventListener( "DOMContentLoaded", ( e ) => {
+
+    const NewsContainerBox = document.getElementById("NewContainerBox");
+    
+    // Write the news headlines
+    Object.keys(NewsData).forEach( dateKey => {
+        writeHeadline( NewsContainerBox, dateKey, NewsData[dateKey]);
+    });
+});
+
+/**
+ * @function writeHeadline
+ * @param {Object} obj <ul> object to add the new <li>
+ * @param {string} date the date string in MM/DD/YYYY
+ * @param {string} line the news headline
+ * @description creates a headline in the obj that was given to it
+ */
+function writeHeadline( obj, date, line ) {
+    var li = document.createElement("li"),
+        datei = document.createElement("i");
+
+    // set the styles & innerHTML
+    li.setAttribute("class", "news-li");
+    datei.innerHTML = date;
+    li.innerHTML = `${datei.outerHTML}: ${line}`;
+    
+    // append the new list item
+    obj.appendChild(li);
+}
