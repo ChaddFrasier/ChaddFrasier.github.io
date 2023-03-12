0bb31e6d9e4b570f8034d87a73e7670baa45af2f
Chadd Frasier <cmf339@nau.edu>
2023-03-12 12:44:08 -0700

    updated reference to redux generator

--- res/src/index.js
+++ res/src/index.js
@@ -8,6 +8,7 @@
  *      There should not be two of the same dates ever so that is why they are the keys
  */
 const NewsData = {
+    '03/12/2023': "Public Release of redux-feature-generator 1.4.0.",
     '08/22/2022': "Technical lead for the 8bit bootloader project at MCU8 Applications.",
     '08/07/2021': "First day starting at Microchip.",
     '06/28/2021': "Presenting at Planet Data 2021 for PIE.",
